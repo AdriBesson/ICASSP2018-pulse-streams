41882e2555f2ce4b8d7f262098fefbbd1d290671
Adrien Besson <adrien.besson@epfl.ch>
2017-10-13 08:51:35 +0200

    Update noisy and invivo

--- Script_reproduce_invivo_experiment.m
+++
@@ -1,6 +0,0 @@
-% Regenerate the noiseless experiment
-addpath(genpath('src'));
-cd('invivo');
-
-%Script
-run('Script_invivo_reconstruction.m')
--- Script_reproduce_noisy_experiment.m
+++
@@ -1,4 +0,0 @@
-% Regenerate the noisy experiment
-cd('Noisy recovery');
-
-run('Script_results_1exp_noisy.m')
